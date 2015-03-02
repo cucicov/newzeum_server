ec0aabe330d1ec07b4b57c8b15ef4562cd50bbcf
cucicov <cucicov@gmail.com>
2015-03-03 00:21:29 +0200

    aaa

--- src/test/java/newzeum/SimpleTest.java
+++ src/test/java/newzeum/SimpleTest.java
@@ -10,16 +10,16 @@ import org.junit.Test;
 
 public class SimpleTest {
 
-	@Test
-	public void testCreateImage(){
-		BufferedImage img = null;
-		try {
-			img = ImageIO.read(new File("images/test2.jpg"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail();
-		}
-		Assert.assertTrue(img != null);
-	}
+//	@Test
+//	public void testCreateImage(){
+//		BufferedImage img = null;
+//		try {
+//			img = ImageIO.read(new File("images/test2.jpg"));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			Assert.fail();
+//		}
+//		Assert.assertTrue(img != null);
+//	}
 	
 }
